5d840b3c2112f5b6d780e20b7de3207089d4dc3b
Haykaz810 <muradyanhaykaz@gmail.com>
2020-08-22 15:07:55 +0400

    add statics files

--- restaurant_reservations/settings.py
+++ restaurant_reservations/settings.py
@@ -11,9 +11,14 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [STATIC_DIR, ]
+VENV_PATH = os.path.dirname(BASE_DIR)
+STATIC_ROOT = os.path.join(VENV_PATH, 'static_root')
 
 
 # Quick-start development settings - unsuitable for production
@@ -40,6 +45,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'accounts',
     'home.apps.HomeConfig',
+    'static'
 
 ]
 
@@ -129,3 +135,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
